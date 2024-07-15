059ac7de7822ef9d30a06ce461ecbc7ff9b94b22
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-07-16 05:47:51 +0600

    add: mongodb & save user api with bcrypt

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6fu63x8.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("cashJetDB").collection("users");
+
+    // save users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const isExist = await usersCollection.findOne({ email: user.email });
+      if (isExist) {
+        res.send({ message: "User already exist", insertedId: null });
+        return;
+      }
+
+      //   const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(user.pin, 10);
+      user.pin = hashedPassword;
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Cash jet server is running");
